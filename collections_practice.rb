7aaa0ac21f99c54d718cc19daa9ee02e5e5af9fb
doulighan <doulighan@gmail.com>
2017-04-12 13:49:54 -0400

    need to learn hashes before I finish the last 4 methods

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
-# your code goes here
+def begins_with_r(array)
+	array.all? { |el| el.start_with?("r") }
+end
+
+def contain_a(array)
+	has_a = []
+	array.each do |el|
+		has_a << el if el.include?("a")
+	end
+	has_a
+end
+
+def first_wa(array)
+	array.detect { |el| el.to_s.include?("wa") }
+end
+
+def remove_non_strings(array)
+	array.delete_if { |el| el != el.to_s }
+end
+
+# need to learn hashes first
+
+def count_elements
+end
+
+def merge_data
+end
+
+def find_cool
+end
+
+def organize_schools
+end
+
